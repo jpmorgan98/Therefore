4f5fd55e10019c01b5b5b4b12cebc65fefef441e
Joanna Piper Morgan (jonsey) <jpmorgan98@gmail.com>
2024-01-08 17:55:32 -0800

    wip

---
+++ src/builder_scb.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "base_mats.h"
+#include "legendre.h"
+
+
+
+void BuildA_Sweep(){
+    // breif builds the large A mat as offsets within an angular sweep
+}
+ 
+void BuildB_Sweep(std::vector<double> &b, std::vector<cell> &cells, problem_space ps){
+    // breif: builds the RHS vector for a source itteration
+
+}
+
+// if an
