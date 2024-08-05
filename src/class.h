6af7370f37687d9535507f86f2020a7e3961d61a
Joanna Piper Morgan <morgjack@oregonstate.edu>
2024-08-05 13:10:05 -0700

    removing mms from class

--- src/class.h
+++ src/class.h
@@ -1,4 +1,4 @@
-#include "mms.h"
+//#include "mms.h"
 using namespace std;
 
 
@@ -46,10 +46,6 @@ class problem_space{
         double velocity;
         double L;
         double t_init;
-
-        bool mms_bool;
-        mms manSource; // source for the method of manufactured solution
-        double ds;
 
         std::vector<double> weights;
         std::vector<double> angles;
@@ -131,8 +127,6 @@ class problem_space{
                 return(0.0);
             } else if (boundary_conditions[side] == 1){ //reflecting
                 return( reflectingBC(side, group, angle) ); // manual alteration for reeds, change back
-            } else if (boundary_conditions[side] == 3){ //mms
-                return ( mms_boundary(side, group, angle, hn) );
             } else {
                 bound_warn();
                 return(0.0);
@@ -153,47 +147,47 @@ class problem_space{
         int index_left;
         int index_right;
 
-        if (mms_bool){
-            std::vector<double> temp;
-
+        //if (mms_bool){
+        //    std::vector<double> temp;
+//
+        //    for (int j=0; j<N_angles; j++){
+//
+        //        temp = manSource.group1af(0, dx, dt*time_val, dt, angles[j]);
+//
+        //        af_left_bound[0*2*N_angles + 2*j  ] = temp[1];
+        //        af_left_bound[0*2*N_angles + 2*j + 1] = temp[3];
+        //        
+        //        temp = manSource.group1af(L, dx, dt*time_val, dt, angles[j]);
+//
+        //        af_right_bound[0*2*N_angles + 2*j  ] = temp[0];
+        //        af_right_bound[0*2*N_angles + 2*j + 1] = temp[2];
+        //        
+        //        temp = manSource.group2af(0, dx, dt*time_val, dt, angles[j]);
+//
+        //        af_left_bound[1*2*N_angles + 2*j  ] = temp[1];
+        //        af_left_bound[1*2*N_angles + 2*j + 1] = temp[3];
+//
+        //        temp = manSource.group2af(L, dx, dt*time_val, dt, angles[j]);
+        //        
+        //        af_right_bound[1*2*N_angles + 2*j  ] = temp[0];
+        //        af_right_bound[1*2*N_angles + 2*j + 1] = temp[2];
+        //    }
+//
+        //} else {
+        for (int g=0; g<N_groups; g++){
             for (int j=0; j<N_angles; j++){
 
-                temp = manSource.group1af(0, dx, dt*time_val, dt, angles[j]);
+                index_left  = g*(SIZE_groupBlocks) + 4*j + 2;
+                index_right = ((N_cells-1)*(SIZE_cellBlocks) + g*(SIZE_groupBlocks) + 4*j) + 3;
 
-                af_left_bound[0*2*N_angles + 2*j  ] = temp[1];
-                af_left_bound[0*2*N_angles + 2*j + 1] = temp[3];
-                
-                temp = manSource.group1af(L, dx, dt*time_val, dt, angles[j]);
+                outofbounds_check(index_right, aflux_last);
+                outofbounds_check(index_left, aflux_last);
 
-                af_right_bound[0*2*N_angles + 2*j  ] = temp[0];
-                af_right_bound[0*2*N_angles + 2*j + 1] = temp[2];
-                
-                temp = manSource.group2af(0, dx, dt*time_val, dt, angles[j]);
-
-                af_left_bound[1*2*N_angles + 2*j  ] = temp[1];
-                af_left_bound[1*2*N_angles + 2*j + 1] = temp[3];
-
-                temp = manSource.group2af(L, dx, dt*time_val, dt, angles[j]);
-                
-                af_right_bound[1*2*N_angles + 2*j  ] = temp[0];
-                af_right_bound[1*2*N_angles + 2*j + 1] = temp[2];
-            }
-
-        } else {
-            for (int g=0; g<N_groups; g++){
-                for (int j=0; j<N_angles; j++){
-
-                    index_left  = g*(SIZE_groupBlocks) + 4*j + 2;
-                    index_right = ((N_cells-1)*(SIZE_cellBlocks) + g*(SIZE_groupBlocks) + 4*j) + 3;
-
-                    outofbounds_check(index_right, aflux_last);
-                    outofbounds_check(index_left, aflux_last);
-
-                    af_left_bound[g*N_groups + j] = aflux_last[index_left];
-                    af_right_bound[g*N_groups +j] = aflux_last[index_right];
-                }
+                af_left_bound[g*N_groups + j] = aflux_last[index_left];
+                af_right_bound[g*N_groups +j] = aflux_last[index_right];
             }
         }
+        //}
     }
 
     void bound_warn(){
