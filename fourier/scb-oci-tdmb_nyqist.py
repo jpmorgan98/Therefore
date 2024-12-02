27d81741e2a244fedc9204664fc5ceb2f4c98fdc
Joanna Piper Morgan (jonsey) <jpmorgan98@gmail.com>
2024-12-01 18:11:20 -0800

    adding more plotting scripts

---
+++ fourier/scb-oci-tdmb_nyqist.py
@@ -0,0 +1,181 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import math
+
+
+np.set_printoptions(edgeitems=30, linewidth=100000, 
+    formatter=dict(float=lambda x: "%.3g" % x))
+
+N_angle = 8
+
+[angles, weights] = np.polynomial.legendre.leggauss(N_angle)
+
+dx = .1
+dt = 1000
+v = 5
+sigma = 4.0
+sigma_s = sigma*.5
+
+N_l = 10000
+lam = np.pi*np.linspace(0,2,N_l)
+
+i = complex(0,1)
+
+tol = 1e-13
+
+def Rblockpos(a):
+    return(np.array([[angles[a]/2 + dx/2*sigma, -angles[a]/2, dx/(2*v*dt), 0],
+                     [angles[a]/2, angles[a]/2 + dx/2*sigma, 0, dx/(2*v*dt)],
+                     [-dx/(v*dt), 0, dx/(v*dt)+angles[a]/2+dx/2*sigma, -angles[a]/2],
+                     [0, -dx/(v*dt), angles[a]/2, dx/(v*dt) + angles[a]/2 + dx/2*sigma]])
+    )
+
+def Rblockneg(a):
+    return(np.array([[-angles[a]/2 + dx/2*sigma, -angles[a]/2, dx/(2*v*dt), 0],
+                     [angles[a]/2, -angles[a]/2 + dx/2*sigma, 0, dx/(2*v*dt)],
+                     [-dx/(v*dt), 0, dx/(v*dt)-angles[a]/2+dx/2*sigma, -angles[a]/2],
+                     [0, -dx/(v*dt), angles[a]/2, dx/(v*dt) - angles[a]/2 + dx/2*sigma]])
+    )
+
+def RBuild():
+    A = np.zeros((4*N_angle, 4*N_angle),dtype=np.complex_)
+    for a in range(N_angle):
+        if   angles[a] > 0:
+            A[a*4:(a+1)*4,a*4:(a+1)*4] = Rblockpos(a)
+        elif angles[a] < 0:
+            A[a*4:(a+1)*4,a*4:(a+1)*4] = Rblockneg(a)
+    return(A)
+
+
+
+def Eblockpos(a,l):
+    return(np.array([[0,0,0,0],
+                     [angles[a]*np.exp(-i*lam[l]*sigma*dx),0,0,0],
+                     [0,0,0,0],
+                     [0,0,angles[a]*np.exp(-i*lam[l]*sigma*dx),0]],dtype=np.complex_)
+    )
+def Eblockneg(a,l):
+    return(np.array([[0,-angles[a]*np.exp(i*lam[l]*sigma*dx),0,0],
+                     [0,0,0,0],
+                     [0,0,0,-angles[a]*np.exp(i*lam[l]*sigma*dx)],
+                     [0,0,0,0]],dtype=np.complex_)
+    )
+
+def EBuild(l):
+    A = np.zeros((4*N_angle, 4*N_angle),dtype=np.complex_)
+    for a in range(N_angle):
+        if   angles[a] > 0:
+            A[a*4:(a+1)*4,a*4:(a+1)*4] = Eblockpos(a,l)
+        elif angles[a] < 0:
+            A[a*4:(a+1)*4,a*4:(a+1)*4] = Eblockneg(a,l)
+    return(A)
+
+
+
+def Sbuild():
+    N = N_angle
+    S = np.zeros((4*N_angle, 4*N_angle),dtype=np.complex_)
+    beta = dx*sigma_s/4
+
+    for p in range(N):
+        for j in range(N):
+            S[p*4,   j*4]   = beta*weights[j]
+            S[p*4+1, j*4+1] = beta*weights[j]
+            S[p*4+2, j*4+2] = beta*weights[j]
+            S[p*4+3, j*4+3] = beta*weights[j]
+    return(S)
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = 'Progress:', suffix = 'Complete', decimals = 1, length = 25, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
+def compute():
+    spec_rad = np.zeros(N_l)
+    spec_rad_complex = np.zeros(N_l, dtype=np.complex_)
+    S = Sbuild()
+    R = RBuild()
+
+    RmSinv = np.linalg.inv(R-S)
+
+    eig_vals = np.zeros((1), dtype=np.complex_)
+
+    for l in range(N_l):
+        E = EBuild(l)
+        T = np.matmul(RmSinv, E)
+        eig_val, stand_eig_mat = np.linalg.eig(T)
+
+        eig_vals = np.append(eig_vals, eig_val, axis=0)
+
+    # plot the complex numbers 
+    #plt.plot(eig_vals.real, eig_vals.imag, 'k.') 
+    #plt.ylabel('Imaginary') 
+    #plt.xlabel('Real') 
+    #plt.show()
+
+    #mags = np.abs(eig_vals)
+
+    #fig = plt.figure()
+    #ax = fig.add_subplot(projection='3d')
+    #ax.scatter(eig_vals.real, eig_vals.imag, mags)
+    #plt.show()
+
+    #x = np.linspace(np.min(eig_vals.real), np.max(eig_vals.real), 20)
+    #y = np.linspace(np.min(eig_vals.imag), np.max(eig_vals.imag), 20)
+
+    #heatmap, blah, blah = np.histogram2d(x, y, weights=mags)
+
+    #plt.clf()
+    #plt.imshow(heatmap)
+    #plt.show()
+
+    #from matplotlib import cm
+    #f = plt.figure()
+
+    #ax = f.add_subplot(111)
+    #ax.pcolormesh(eig_vals.real,eig_vals.imag, mags, cmap = cm.viridis)
+    #f.show()
+
+    #print(np.max(np.abs(eig_vals)))
+
+    return(np.max(np.abs(eig_vals)))
+#def plot_dt():
+
+
+if __name__ == '__main__':
+
+    dt_range = np.array([100, 10, 1, 0.1])
+    mfp = np.array([0.1, 1.0, 10])
+    c = 1
+    sigma_s = c*sigma
+
+    for m in range(dt_range.size):
+        for j in range(mfp.size):
+            dt = dt_range[m]
+            dx = mfp[j]/sigma
+
+            spec = compute()
+
+            print("dt {}, mfp {}, spec rad {}".format(dt, mfp[j], spec))
+
+    # plotting 
